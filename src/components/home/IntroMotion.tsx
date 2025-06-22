ac0d0e8ad9c67b212fa031c6b3ad173bd44295a1
jernsth <jonashermsen@gmx.de>
2025-06-22 22:41:54 +0200

    Motion Bug fix for mobile devices

--- src/components/home/IntroMotion.tsx
+++ src/components/home/IntroMotion.tsx
@@ -3,20 +3,23 @@
 import { motion } from "framer-motion";
 import { ProfileIntro } from "./ProfileIntro";
 import { ProjectsIntro } from "./ProjectsIntro";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export function IntroMotion() {
+    const isMobile = useIsMobile()
     return (
         <div className="flex flex-row justify-center items-start gap-10 mt-10">
             <motion.div
-                initial={{ opacity: 0, x:20, y: -50 }}
-                animate={{ opacity: 1, x:20, y: 0 }}
+                initial={{ opacity: 0, x: isMobile ? 0 : 20, y: -50 }}
+                animate={{ opacity: 1, x: isMobile ? 0 : 20, y: 0 }}
                 transition={{ duration: 0.6, ease: "easeOut" }}
                 className="max-w-xl"
             >
                 <ProfileIntro />
             </motion.div>
 
-            <motion.div
+            {!isMobile && (
+                <motion.div
                 initial={{ opacity: 0, x: 120 }}
                 animate={{ opacity: 1, x: 90 }}
                 transition={{ duration: 0.6, ease: "easeOut", delay: 0.4 }}
@@ -24,6 +27,7 @@ export function IntroMotion() {
             >
                     <ProjectsIntro />
             </motion.div>
+            )}
         </div>
     );
 }
