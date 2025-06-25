045a7f60a12834fdadd79fd0b1b0c9c25e9f3c89
jernsth <jonashermsen@gmx.de>
2025-06-25 14:46:52 +0200

    Motion Bug fix for mobile devices

--- src/components/home/IntroMotion.tsx
+++ src/components/home/IntroMotion.tsx
@@ -13,24 +13,22 @@ export function IntroMotion() {
     return (
         <div className="flex flex-row justify-center items-start gap-10 mt-10">
             <motion.div
-                initial={{ opacity: 0, x: isMobile ? 0 : 20, y: -50 }}
-                animate={{ opacity: 1, x: isMobile ? 0 : 20, y: 0 }}
-                transition={{ duration: 0.6, ease: "easeOut" }}
+                initial={{opacity: 0, x: isMobile ? 0 : 20, y: -50}}
+                animate={{opacity: 1, x: isMobile ? 0 : 20, y: 0}}
+                transition={{duration: 0.6, ease: "easeOut"}}
                 className="max-w-xl"
             >
-                <ProfileIntro />
+                <ProfileIntro/>
             </motion.div>
 
-            {!isMobile && (
-                <motion.div
-                    initial={{ opacity: 0, x: 120 }}
-                    animate={{ opacity: 1, x: 90 }}
-                    transition={{ duration: 0.6, ease: "easeOut", delay: 0.4 }}
-                    className="w-[200px] h-[100px] relative"
-                >
-                    <ProjectsIntro />
-                </motion.div>
-            )}
+            <motion.div
+                initial={{opacity: 0, x: 120}}
+                animate={{opacity: 1, x: 90}}
+                transition={{duration: 0.6, ease: "easeOut", delay: 0.4}}
+                className="w-[200px] h-[100px] relative lg:block hidden"
+            >
+                <ProjectsIntro/>
+            </motion.div>
         </div>
     );
 }
