d18248fe7642cf0cf51293664460d182c4d28e7d
jernsth <jonashermsen@gmx.de>
2025-06-25 15:49:36 +0200

    Intro to Socials on Homepage

--- src/components/home/IntroMotion.tsx
+++ src/components/home/IntroMotion.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { ProfileIntro } from "./ProfileIntro";
 import { ProjectsIntro } from "./ProjectsIntro";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { SocialsIntro } from "./SocialsIntro";
 
 export function IntroMotion() {
     const isMobile = useIsMobile();
@@ -11,25 +12,33 @@ export function IntroMotion() {
     if (isMobile === undefined) return null; // Or a loading spinner
 
     return (
-        <div className="flex flex-row justify-center items-start gap-10 mt-10">
-            <motion.div
-                initial={{opacity: 0, x: isMobile ? 0 : 20, y: -50}}
-                animate={{opacity: 1, x: isMobile ? 0 : 20, y: 0}}
-                transition={{duration: 0.6, ease: "easeOut"}}
+        <div className="flex flex-col items-center gap-y-4 mt-10">
+            <div className="flex flex-row justify-center items-start gap-x-10">
+                <motion.div
+                    initial={{opacity: 0, x: isMobile ? 0 : 20, y: -50}}
+                    animate={{opacity: 1, x: isMobile ? 0 : 20, y: 0}}
+                    transition={{duration: 0.6, ease: "easeOut"}}
+                    className="max-w-xl"
+                >
+                    <ProfileIntro />
+                </motion.div>
+
+                <motion.div
+                    initial={{opacity: 0, x: 120}}
+                    animate={{opacity: 1, x: 90}}
+                    transition={{duration: 0.6, ease: "easeOut", delay: 0.4}}
+                    className="w-[200px] h-[100px] relative lg:block hidden"
+                >
+                    <ProjectsIntro />
+                </motion.div>
+            </div>
+            <div
                 className="max-w-xl"
             >
-                <ProfileIntro/>
-            </motion.div>
-
-            <motion.div
-                initial={{opacity: 0, x: 120}}
-                animate={{opacity: 1, x: 90}}
-                transition={{duration: 0.6, ease: "easeOut", delay: 0.4}}
-                className="w-[200px] h-[100px] relative lg:block hidden"
-            >
-                <ProjectsIntro/>
-            </motion.div>
+                <SocialsIntro />
+            </div>
         </div>
     );
+
 }
 
---
+++ src/components/home/SocialsIntro.tsx
@@ -0,0 +1,64 @@
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { FaLinkedin } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+import { motion } from "framer-motion";
+
+
+// import { Card, CardContent } from "@/components/ui/card";
+// import { FaFileDownload } from "react-icons/fa";
+// FOR FUTURE USE: Uncomment to add a resume download card
+/*
+            <Card className="p-1 hover:scale-105 cursor-pointer duration-200 ease-in-out rounded-lg">
+                <CardContent className="flex items-center justify-center">
+                    <div className="flex gap-2 items-center font-semibold">
+                        <FaFileDownload size={20}/>
+                        <a>Resume</a>
+                    </div>
+                </CardContent>
+            </Card>
+ */
+
+export function SocialsIntro() {
+    return (
+        <div className="flex items-center gap-4">
+            <motion.div
+                initial={{opacity: 0, x: 200}}
+                animate={{opacity: 1, x: 0}}
+                transition={{duration: 1, ease: "easeOut", delay: 0.8, type: "spring"}}
+                className="max-w-xl"
+            >
+                <a target="_blank" rel="noopener noreferrer" href="https://www.github.com/jernsth">
+                    <SiGithub size={24}
+                              className="hover:scale-115 cursor-pointer duration-200 ease-in-out"
+                    />
+                </a>
+            </motion.div>
+            <motion.div
+                initial={{opacity: 0, x: 200}}
+                animate={{opacity: 1, x: 0}}
+                transition={{duration: 1, ease: "easeOut", delay: 1, type: "spring"}}
+                className="max-w-xl"
+            >
+                <a target="_blank" rel="noopener noreferrer"
+                   href="https://www.linkedin.com/in/jonas-hermsen-61b53a142/">
+                    <FaLinkedin size={24}
+                                className="hover:scale-115 cursor-pointer duration-200 ease-in-out"
+                    />
+                </a>
+            </motion.div>
+            <motion.div
+                initial={{opacity: 0, x: 200}}
+                animate={{opacity: 1, x: 0}}
+                transition={{duration: 1, ease: "easeOut", delay: 1.2, type: "spring"}}
+                className="max-w-xl"
+            >
+                <a href="mailto:jonashermsen@gmx.de">
+                    <MdMail size={24}
+                            className="hover:scale-115 cursor-pointer duration-200 ease-in-out"
+                    />
+                </a>
+            </motion.div>
+        </div>
+    );
+
+}
