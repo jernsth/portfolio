a7891163769a3c73f9832e666d92ebb68dc43e02
j.hermsen <j.hermsen@m3connect.de>
2026-03-06 15:41:21 +0100

    update experience

--- src/components/home/Education.tsx
+++ src/components/home/Education.tsx
@@ -5,17 +5,17 @@ import {motion} from "framer-motion";
 
 const educationItems: TimelineItem[] = [
     {
-        title: "Abitur - Konrad-Duden Gymnasium",
-        subtitle: "Wesel, Mai 2018",
-        description: "Focus: Mathematics, Computer Science, English",
-        date: "2018",
-        type: "education",
-    },
-    {
         title: "Computer Science b.Sc. - RWTH Aachen University",
         subtitle: "Aachen, Januar 2026 (expected)",
         description: "Focus: Software Engineering, System Design & Network Technologies",
         date: "2026",
+        type: "education",
+    },
+    {
+        title: "Abitur - Konrad-Duden Gymnasium",
+        subtitle: "Wesel, Mai 2018",
+        description: "Focus: Mathematics, Computer Science, English",
+        date: "2018",
         type: "education",
     },
 ];
